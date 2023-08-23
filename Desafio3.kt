935ac8f38aae434a29abdce4e56bd92feb9573c8
Pablo-Lage-Carral <pablito.lage@hotmail.com>
2023-08-23 20:41:57 -0300

    Desafio 3 preenchido

---
+++ Desafio3.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var nome: String = "Pablo Lage Carral"
+    var idade: Int = 21
+    var email: String = "pablito.lage@hotmail.com"
+    var disponivel: Boolean = true
+
+    println("O $nome de $idade com o usuario $email esta ativo: $disponivel")
+
+
+
+}
